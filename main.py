1acc23245e5204b93c9a40cd58585de5b003368b
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-17 15:44:48 -0700

    extracted the colors from the hirst painting

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Painting Project')
+# import colorgram
+#
+# colors = colorgram.extract('hirst_img.jpg', 20)
+#
+# rgb_colors = []
+# for color in colors:
+#     rgb_formatted = (color.rgb.r, color.rgb.g, color.rgb.b)
+#     rgb_colors.append(rgb_formatted)
+#
+# print(rgb_colors)
+
+color_list = [(62, 100, 150), (240, 83, 25), (175, 52, 34), (140, 30, 46), (75, 49, 41), (108, 171, 202),
+              (174, 44, 50), (175, 180, 34), (237, 197, 68), (245, 214, 1), (226, 127, 58), (87, 34, 77),
+              (85, 61, 39), (179, 146, 169), (99, 130, 159), (171, 188, 178)]
