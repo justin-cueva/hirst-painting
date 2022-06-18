f7ee83652659baf5c82e973a96d6281125fea291
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-18 09:12:18 -0700

    created our painting with the colors extracted

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import turtle
+from turtle import Screen
+import random
 # import colorgram
 #
 # colors = colorgram.extract('hirst_img.jpg', 20)
@@ -12,3 +15,25 @@
 color_list = [(62, 100, 150), (240, 83, 25), (175, 52, 34), (140, 30, 46), (75, 49, 41), (108, 171, 202),
               (174, 44, 50), (175, 180, 34), (237, 197, 68), (245, 214, 1), (226, 127, 58), (87, 34, 77),
               (85, 61, 39), (179, 146, 169), (99, 130, 159), (171, 188, 178)]
+
+screen = Screen()
+tim = turtle.Turtle()
+
+screen.colormode(255)
+tim.speed('fastest')
+tim.penup()
+tim.right(135)
+tim.forward(325)
+tim.left(135)
+for _ in range(10):
+    for _ in range(10):
+        tim.dot(20, random.choice(color_list))
+        tim.forward(50)
+    tim.left(90)
+    tim.forward(50)
+    tim.left(90)
+    tim.forward(50 * 10)
+    tim.left(180)
+
+
+turtle.exitonclick()
